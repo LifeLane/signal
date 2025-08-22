72885b2e8db9f5c2a928cacdcc0230eb2e382dfb
Rajib Singh <thelostvedas@gmail.com>
2025-08-22 08:01:21 +0000

    make this for ant domain otherwise i have to do this again and again and

--- next.config.ts
+++ next.config.ts
@@ -13,42 +13,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
       {
-        protocol: 'https',
-        hostname: 's.yimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.zenfs.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.coindesk.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.coindesk.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.investopedia.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image.cnbcfm.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'i.insider.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'editorial.fxstreet.com',
-      },
+        protocol: 'http',
+        hostname: '**',
+      }
     ],
   },
 };
